46a06db12bfc420ea57e50d060491fc2839e21d1
Dave Baker <dave@dsb3.com>
2023-12-26 13:28:48 +0000

    LWT

--- podman-run.sh
+++ podman-run.sh
@@ -1,2 +1,2 @@
-podman run --rm -ti -e MQTTHOST -e MQTTPORT -e MQTTUSER -e MQTTPASS -e HARVEYUSER -e HARVEYPASS  dsb3/harvey2mqtt
+podman run --rm -ti -e MQTTHOST -e MQTTPORT -e MQTTUSER -e MQTTLWT -e MQTTPASS -e HARVEYUSER -e HARVEYPASS  dsb3/harvey2mqtt
 
